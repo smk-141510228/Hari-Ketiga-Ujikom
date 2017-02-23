1aca45fe2e1b84749439ec0a22c2feee172d7466
smk-141510228 <smk-141510228>
2017-02-23 15:03:17 +0700

    Hari Ketiga Ujikom

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+        <div class="col-10-0">
+            <div class=" panel-default">
+           
+             <center><font size="5" face="comic sans ms">SELAMAT DATANG DI APLIKASI PENGGAJIAN</font><br></center></div>
+             <center><img src="index.jpg"></center>
+   
+    </div>
+</div>
+@endsection
---
+++ storage/framework/views/758c7ac12dd0a7565f8dc326b3d9b6f013aeca53.php
@@ -0,0 +1,13 @@
+<?php $__env->startSection('content'); ?>
+
+        <div class="col-10-0">
+            <div class=" panel-default">
+           
+             <center><font size="5" face="comic sans ms">SELAMAT DATANG DI APLIKASI PENGGAJIAN</font><br></center></div>
+             <center><img src="index.jpg"></center>
+   
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
